0b06b3598a263c053efdea52525b9ef23b3cbc82
amds <450076077@qq.com>
2017-12-14 18:42:56 +0800

    add client and server

---
+++ gpu_client.py
@@ -0,0 +1,49 @@
+import gpustat
+import json
+import argparse
+import zmq
+
+def get_gpu_stat_json():
+    gpustats = gpustat.GPUStatCollection.new_query()
+    output = gpustats.jsonify()
+    return json.dumps(output)
+
+class TransClient:
+    def __init__(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUSH)
+
+    def start(self, host = 'localhost', port = 5558):
+        self.host = host
+        self.port = port
+        server_info = "tcp://" + self.host + ":" + str(self.port)
+        self.socket.connect(server_info)
+        print("connected to server: " + server_info)
+
+    def close(self):
+        self.socket.close()
+
+    def send(self, msg):
+        self.socket.send(msg)
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='localhost',
+                    help='host')
+    parser.add_argument('--port', type=int, default=5558,
+                    help='port')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    opt = parse_opt()
+
+    client = TransClient()
+    client.start(opt.host, opt.port)
+
+    while True:
+        try:
+            stat = get_gpu_stat_json()
+            client.send(stat)
+        except KeyboardInterrupt:
+            print("exit")
---
+++ server.py
@@ -0,0 +1,70 @@
+import json
+import argparse
+import zmq
+import threading
+import time
+import os
+
+class TransServer:
+
+    def setup(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PULL)
+        self.socket.bind('tcp://*:5558')
+        print('started server')
+        self.stats = {}
+
+        self.thread = threading.Thread(target=self.run)
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+    def run(self):
+        while True:
+            self.handle()
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.socket.recv()
+        dict_stat = json.loads(self.data)
+        hostname = dict_stat['hostname']
+        dict_stat["time"] = time.time()
+        self.stats[hostname] = dict_stat
+
+    def print_stat(self):
+        print("======================================================")
+        keys = self.stats.keys()
+        for key in keys:
+            if time.time() - self.stats[key]["time"] > 10:
+                del self.stats[key]
+            else:
+                print("hostname: {}".format(key))
+                print("------------------------------------------------------")
+                for gpu in self.stats["gpus"]:
+                    print("name: {} | memory: {}/{} | used:{}% | power:{}/{} | temp: {}C".format(
+                        gpu["name"],
+                        gpu["memory.used"],
+                        gpu["memory.total"],
+                        gpu["utilization.gpu"],
+                        gpu["power.draw"],
+                        gpu["enforced.power.limit"],
+                        gpu["temperature.gpu"]
+                ))
+                print("======================================================")
+        if len(keys) == 0:
+            print("======================================================")
+
+
+if __name__ == "__main__":
+
+    server = TransServer()
+    server.setup()
+
+    os.system("clear")
+    while True:
+        try:
+            server.print_stat()
+            time.sleep(1)
+            os.system("clear")
+        except KeyboardInterrupt:
+            print("exit")
+
