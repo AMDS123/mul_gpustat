8da61e7bfc92e0b49f264911206bb15c0842baf4
amds <450076077@qq.com>
2017-12-14 22:02:29 +0800

    add client and server

--- server.py
+++ server.py
@@ -31,19 +31,20 @@ class TransServer:
         self.stats[hostname] = dict_stat
 
     def print_stat(self):
-        print("====================================================================")
+        lines = []
+        lines.append("====================================================================\n")
         keys = self.stats.keys()
         for key in keys:
             if time.time() - self.stats[key]["time"] > 10:
                 del self.stats[key]
             else:
-                print("hostname: {}".format(key))
-                #     "===================================================================="
-                print("--------------------------------------------------------------------")
+                lines.append("hostname: {}".format(key))
+                #     "====================================================================\n"
+                print("--------------------------------------------------------------------\n")
                 gpus = self.stats[key]["gpus"]
                 for index in range(len(gpus)):
                     gpu = gpus[index]
-                    print("[{}] | {:3d}% | {:4d}W/{:3}W | {}C | {:5d}/{:5d} MB | {}".format(
+                    lines.append("[{}] | {:3d}% | {:4d}W/{:3}W | {}C | {:5d}/{:5d} MB | {}\n".format(
                         index,
                         gpu["utilization.gpu"],
                         gpu["power.draw"],
@@ -53,9 +54,12 @@ class TransServer:
                         gpu["memory.total"],
                         gpu["name"]
                 ))
-                print("====================================================================")
+                print("====================================================================\n")
         if len(keys) == 0:
-            print("====================================================================")
+            print("====================================================================\n")
+
+        with open("stat.txt", "w") as f:
+            f.writelines(lines)
 
 
 if __name__ == "__main__":
