e7d240bd7d3d495074e488753a5fb1f771903288
amds <450076077@qq.com>
2018-01-11 16:16:53 +0800

    add client and server

---
+++ ip_client.py
@@ -0,0 +1,101 @@
+import json
+import argparse
+import zmq
+import time
+import socket
+import requests
+
+def get_hostname():
+    hostname = socket.gethostname()
+    return hostname
+
+def get_remote_ip():
+    ip = ''
+    try:
+        url = r'http://1212.ip138.com/ic.asp'
+        r = requests.get(url)
+        txt = r.text
+        ip = txt[txt.find("[") + 1: txt.find("]")]
+    except:
+        print('ip get error')
+    return ip
+
+
+def get_local_ip():
+    ip = ''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('www.baidu.com', 80))
+        ip = s.getsockname()[0]
+    except:
+        print('ip get error')
+    finally:
+        s.close()
+    return ip
+
+
+def get_stat_json(opt):
+    output = {}
+    output['hostname'] = get_hostname()
+    output['query_time'] = time.time()
+    if opt.get_remote_ip == 1:
+        output['remote_ip'] = get_remote_ip()
+    output['local_ip'] = get_local_ip()
+    return json.dumps(output)
+
+class TransClient:
+    def __init__(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUSH)
+
+    def start(self, host = 'localhost', port = 5559):
+        self.host = host
+        self.port = port
+        self.server_info = "tcp://" + self.host + ":" + str(self.port)
+        self.socket.connect(self.server_info)
+        print("connected to server: " + self.server_info)
+
+    def reconnect(self):
+        self.socket.close()
+        self.socket.connect(self.server_info)
+        print("reconnected to server: " + self.server_info)
+
+    def close(self):
+        self.socket.close()
+
+    def send(self, msg):
+        try:
+            self.socket.send(msg)
+        except:
+            self.reconnect()
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='localhost',
+                    help='host')
+    parser.add_argument('--port', type=int, default=5559,
+                    help='port')
+    parser.add_argument('--socket_timeout', type=int, default=1,
+                        help='socket timeout')
+    parser.add_argument('--get_remote_ip', type=int, default=0,
+                        help='get_remote_ip')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    opt = parse_opt()
+
+    socket.setdefaulttimeout(opt.socket_timeout)
+
+    client = TransClient()
+    client.start(opt.host, opt.port)
+
+    while True:
+        try:
+            stat = get_stat_json(opt)
+            print(stat)
+            client.send(stat)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("exit")
+            exit(0)
---
+++ ip_server.py
@@ -0,0 +1,113 @@
+import json
+import argparse
+import zmq
+import threading
+import time
+import os
+
+class TransServer:
+
+    def setup(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PULL)
+        self.socket.bind('tcp://*:5559')
+        print('started server')
+        self.stats = {}
+
+        self.thread = threading.Thread(target=self.run)
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+        self.thread_ip = threading.Thread(target=self.write_ip)
+        self.thread_ip.setDaemon(True)
+        self.thread_ip.start()
+
+    def run(self):
+        while True:
+            self.handle()
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.socket.recv()
+        dict_stat = json.loads(self.data)
+        hostname = dict_stat['hostname']
+        dict_stat["time"] = time.time()
+        self.stats[hostname] = dict_stat
+
+    def print_stat(self):
+        lines = []
+        lines.append("====================================================================\n")
+        keys = self.stats.keys()
+        for key in keys:
+            run_stat = "RUNNING"
+            if time.time() - self.stats[key]["time"] > 10:
+                run_stat = "STOP"
+
+            local_ip = ""
+            remote_ip = ""
+            if self.stats[key].has_key("local_ip"):
+                local_ip = "[" + self.stats[key]["local_ip"] + "]"
+            if self.stats[key].has_key("remote_ip"):
+                remote_ip = "[" + self.stats[key]["remote_ip"] + "]"
+
+            lines.append("{} ({}) {} {}\n".format(key, run_stat, local_ip, remote_ip))
+            lines.append("====================================================================\n")
+        if len(keys) == 0:
+            print("====================================================================\n")
+
+        print(''.join(lines))
+
+        with open("ip_stat.txt", "w") as f:
+            f.writelines(lines)
+
+    def write_html(self):
+        html = ""
+        html += "<table>"
+        keys = self.stats.keys()
+        for key in keys:
+            run_stat = "RUNNING"
+            if time.time() - self.stats[key]["time"] > 5:
+                run_stat = "STOP"
+
+            local_ip = ""
+            remote_ip = ""
+            if self.stats[key].has_key("local_ip"):
+                local_ip = "[" + self.stats[key]["local_ip"] + "]"
+            if self.stats[key].has_key("remote_ip"):
+                remote_ip = "[" + self.stats[key]["remote_ip"] + "]"
+
+            html += "<tr class='" + run_stat + "'>"
+            html += "<td colspan='6'>{} ({}) {} {}</td>".format(key, run_stat, local_ip, remote_ip)
+            html += "</tr>"
+        html += "</table>"
+        with open("stat.html", "w") as f:
+            f.writelines(html)
+
+    def write_ip(self):
+        while True:
+            keys = self.stats.keys()
+            for key in keys:
+                if self.stats[key].has_key("remote_ip"):
+                    remote_ip = self.stats[key]["remote_ip"]
+                    with open(key + "_ip.txt", "w") as f:
+                        f.write(remote_ip)
+
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+
+    server = TransServer()
+    server.setup()
+
+    os.system("clear")
+    while True:
+        try:
+            server.print_stat()
+            server.write_html()
+            time.sleep(1)
+            os.system("clear")
+        except KeyboardInterrupt:
+            print("exit")
+            exit(0)
+
